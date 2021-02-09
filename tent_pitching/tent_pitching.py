d737271c5306acd5b5f5315301ef63719c30c249
Hendrik Kleikamp <h_klei18@uni-muenster.de>
2021-02-09 12:05:07 +0100

    started implementing DG-scheme
    Refactoring of Function-classes is necessary to facilitate different
    discretizations. Thus far, the implementation is based on CG-Ansatz-functions.

--- tent_pitching/tent_pitching.py
+++ tent_pitching/tent_pitching.py
@@ -1,8 +1,8 @@
-from tent_pitching.grids import SpaceTimeMesh
+from tent_pitching.grids import SpaceTimeGrid
 
 
 def perform_tent_pitching(space_grid, t_max, characteristic_speed, n_max=1000, log=True):
-    space_time_mesh = SpaceTimeMesh(space_grid, t_max, characteristic_speed)
+    space_time_grid = SpaceTimeGrid(space_grid, t_max, characteristic_speed)
 
     n = 0
 
@@ -10,22 +10,22 @@ def perform_tent_pitching(space_grid, t_max, characteristic_speed, n_max=1000, l
         print("Order of tent pitching locations:")
 
     while True and n < n_max:
-        space_time_vertex = space_time_mesh.advancing_front.get_feasible_vertex()
+        space_time_vertex = space_time_grid.advancing_front.get_feasible_vertex()
         if space_time_vertex is None:
             break
 
         if log:
             print(space_time_vertex)
 
-        space_time_mesh.pitch_tent(space_time_vertex)
+        space_time_grid.pitch_tent(space_time_vertex)
 
         n += 1
 
     if log:
         print("Finished tent pitching...")
 
-    if space_time_mesh.advancing_front.get_feasible_vertex() is not None:
+    if space_time_grid.advancing_front.get_feasible_vertex() is not None:
         assert n == n_max
         raise Exception(f"The maximum number of {n_max} tents is reached without finishing the spacetime meshing process!")
 
-    return space_time_mesh
+    return space_time_grid
