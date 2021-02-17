cea1897b92199f6fe193ba6dbf46eaace70cce90
Hendrik Kleikamp <h_klei18@uni-muenster.de>
2021-02-17 15:51:49 +0100

    use numerical flux given to constructor of DG scheme

--- tent_pitching/discretizations/numerical_fluxes.py
+++ tent_pitching/discretizations/numerical_fluxes.py
@@ -6,4 +6,4 @@ class LaxFriedrichsFlux:
         self.lambda_ = lambda_
 
     def __call__(self, u_1, u_2, n):
-        return (self.flux(u_1) + self.flux(u_2)) / 2. * n + self.lambda_ * (u_1 - u_2)
+        return (self.flux(u_1) + self.flux(u_2)) / 2. * n + 1. / (2. * self.lambda_) * (u_1 - u_2)
