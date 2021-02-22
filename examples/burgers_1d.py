fixed numerical fluxes

examples/burgers_1d.py
@@ -31,9 +31,9 @@ grid_operator = GridOperator(space_time_grid, DGFunction,
                              local_time_grid_size=LOCAL_TIME_GRID_SIZE)
 
 
-def u_0_function(x, jump=False):
+def u_0_function(x, jump=True):
     if jump:
-        return 1. * (x <= 0.25)
+        return 1. * (x <= 0.25) + 0.25 * (0.25 < x <= 0.5)
     return 0.5 * (1.0 + np.cos(2.0 * np.pi * x)) * (0.0 <= x <= 0.5) + 0. * (x > 0.5)
 
 

tent_pitching/discretizations/dg.py
@@ -84,13 +84,11 @@ class DiscontinuousGalerkin:
                 delta = tent.get_top_front_value(x) - tent.get_bottom_front_value(x)
                 delta_left = tent.get_top_front_value(x_left) - tent.get_bottom_front_value(x_left)
                 delta_right = tent.get_top_front_value(x_right) - tent.get_bottom_front_value(x_right)
-                val += ((self.numerical_flux(function_value_central, function_value_left, -1.)
-                         + self.numerical_flux(function_value_central, function_value_right, 1.)
+                val += ((self.numerical_flux(function_value_central, function_value_right)
+                         - self.numerical_flux(function_value_left, function_value_central)
                          ) * delta)
-#                val += (self.numerical_flux(function_value_central, function_value_left, -1.) * delta_left
-#                        + self.numerical_flux(function_value_central, function_value_right, 1.) * delta_right)
-                if np.abs(val) >= 1e-3:
-                    print(val, pos)
+#                val += (self.numerical_flux(function_value_central, function_value_right) * delta_right
+#                        - self.numerical_flux(function_value_left, function_value_central) * delta_left)
 
                 vector[pos] = val
                 pos += 1