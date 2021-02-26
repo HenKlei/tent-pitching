85ecb32878096dd5734ca6d4a0564756c46dfcd9
Hendrik Kleikamp <h_klei18@uni-muenster.de>
2021-02-26 14:57:09 +0100

    fixed examples

--- examples/burgers_1d.py
+++ examples/burgers_1d.py
@@ -12,7 +12,7 @@ from tent_pitching.discretizations import DiscontinuousGalerkin
 
 grid = create_uniform_grid(0.33333333)
 T_MAX = 1.
-EPS = 1e-0
+EPS = 2e-0
 
 
 def characteristic_speed(x):
@@ -31,8 +31,8 @@ grid_operator = GridOperator(space_time_grid, DGFunction,
                              local_time_grid_size=LOCAL_TIME_GRID_SIZE)
 
 
-def u_0_function(x, jump=True):
-    if jump:
+def u_0_function(x, jumps=False):
+    if jumps:
         return 1. * (x <= 0.25) + 0.25 * (0.25 < x <= 0.5)
     return 0.5 * (1.0 + np.cos(2.0 * np.pi * x)) * (0.0 <= x <= 0.5) + 0. * (x > 0.5)
 
@@ -54,13 +54,11 @@ def inverse_transformation(u, phi_2, phi_2_dt, phi_2_dx):
     return 2 * u / (1 + np.sqrt(1 - 2 * u * phi_2_dx))
 
 
-ETA_DIRICHLET = 0.#1e-4
-
 discretization = DiscontinuousGalerkin(burgers_flux, burgers_flux_derivative,
                                        inverse_transformation, LOCAL_SPACE_GRID_SIZE,
-                                       LOCAL_TIME_GRID_SIZE, eta_dirichlet=ETA_DIRICHLET)
+                                       LOCAL_TIME_GRID_SIZE)
 u = grid_operator.solve(u_0, discretization)
 
-plot_space_time_function(u, title='Space time solution')
+plot_space_time_function(u, inverse_transformation, title='Space time solution')
 
 plt.show()
--- examples/linear_transport_1d.py
+++ examples/linear_transport_1d.py
@@ -21,7 +21,7 @@ elements = [element0, element1, element2, ]
 grid = Grid(elements)
 T_MAX = 1.
 MU = 1.
-EPS = 1e-6
+EPS = 2e-0
 
 
 def characteristic_speed(x):
@@ -32,17 +32,17 @@ space_time_grid = perform_tent_pitching(grid, T_MAX, characteristic_speed, n_max
 
 plot_1d_space_time_grid(space_time_grid, title='Space time grid obtained via tent pitching')
 
-LOCAL_SPACE_GRID_SIZE = 1e-1
-LOCAL_TIME_GRID_SIZE = 1e-1
+LOCAL_SPACE_GRID_SIZE = 1e-2
+LOCAL_TIME_GRID_SIZE = 1e-2
 
 grid_operator = GridOperator(space_time_grid, DGFunction,
                              local_space_grid_size=LOCAL_SPACE_GRID_SIZE,
                              local_time_grid_size=LOCAL_TIME_GRID_SIZE)
 
 
-def u_0_function(x, jump=True):
-    if jump:
-        return 1. * (x <= 0.25)
+def u_0_function(x, jumps=True):
+    if jumps:
+        return 1. * (x <= 0.2) + 0.5 * (0.2 < x <= 0.5)
     return 0.5 * (1.0 + np.cos(2.0 * np.pi * x)) * (0.0 <= x <= 0.5) + 0. * (x > 0.5)
 
 
@@ -56,23 +56,19 @@ def linear_transport_flux(u):
 
 
 def linear_transport_flux_derivative(u):
-    return 0.
+    return MU
 
 
 def inverse_transformation(u, phi_2, phi_2_dt, phi_2_dx):
-    return u
-    # Should actually be:
-    # return u / (phi_1_prime - phi_2_dx * MU)
+    return u / (1. - phi_2_dx * MU)
 
-
-ETA_DIRICHLET = 1e-5
 
 discretization = DiscontinuousGalerkin(linear_transport_flux, linear_transport_flux_derivative,
                                        inverse_transformation, LOCAL_SPACE_GRID_SIZE,
-                                       LOCAL_TIME_GRID_SIZE, eta_dirichlet=ETA_DIRICHLET)
+                                       LOCAL_TIME_GRID_SIZE)
 
 u = grid_operator.solve(u_0, discretization)
 
-plot_space_time_function(u, title='Space time solution')
+plot_space_time_function(u, inverse_transformation, title='Space time solution')
 
 plt.show()
