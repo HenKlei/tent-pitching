bda890adf8eaed86d829ae24f6cb39b3ed6239f4
Hendrik Kleikamp <h_klei18@uni-muenster.de>
2021-03-01 13:46:10 +0100

    added parametrized Burgers equation

--- examples/burgers_1d_parametrized.py
+++ examples/burgers_1d_parametrized.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from tent_pitching import perform_tent_pitching
-from tent_pitching.grids import create_uniform_grid
+from tent_pitching.grids import Vertex, Element, Grid, create_uniform_grid
 from tent_pitching.visualization import (plot_1d_space_time_grid, plot_space_function,
                                          plot_space_time_function)
 from tent_pitching.operators import GridOperator
@@ -10,11 +10,10 @@ from tent_pitching.functions import DGFunction
 from tent_pitching.discretizations import DiscontinuousGalerkin
 
 
-GLOBAL_SPACE_GRID_SIZE = 0.3333333
-grid = create_uniform_grid(GLOBAL_SPACE_GRID_SIZE)
+grid = create_uniform_grid(0.33333333)
 T_MAX = 1.
-MU = 1.
-EPS = 2e-0
+EPS = 4e-0
+MU = 2.
 
 
 def characteristic_speed(x):
@@ -33,9 +32,9 @@ grid_operator = GridOperator(space_time_grid, DGFunction,
                              local_time_grid_size=LOCAL_TIME_GRID_SIZE)
 
 
-def u_0_function(x, jump=True):
-    if jump:
-        return 1. * (x <= 0.25)
+def u_0_function(x, jumps=False):
+    if jumps:
+        return 1. * (x <= 0.25) + 0.25 * (0.25 < x <= 0.5)
     return 0.5 * (1.0 + np.cos(2.0 * np.pi * x)) * (0.0 <= x <= 0.5) + 0. * (x > 0.5)
 
 
@@ -44,22 +43,21 @@ u_0 = grid_operator.interpolate(u_0_function)
 plot_space_function(u_0, title='Initial condition interpolated to DG space')
 
 
-def linear_transport_flux(u):
-    return MU * u
+def burgers_flux(u):
+    return 0.5 * MU * u**2
 
 
-def linear_transport_flux_derivative(u):
-    return MU
+def burgers_flux_derivative(u):
+    return u * MU
 
 
 def inverse_transformation(u, phi_2, phi_2_dt, phi_2_dx):
-    return u / (1. - phi_2_dx * MU)
+    return 2 * u / (1 + np.sqrt(1 - 2 * u * phi_2_dx * MU))
 
 
-discretization = DiscontinuousGalerkin(linear_transport_flux, linear_transport_flux_derivative,
+discretization = DiscontinuousGalerkin(burgers_flux, burgers_flux_derivative,
                                        inverse_transformation, LOCAL_SPACE_GRID_SIZE,
                                        LOCAL_TIME_GRID_SIZE)
-
 u = grid_operator.solve(u_0, discretization)
 
 plot_space_time_function(u, inverse_transformation, title='Space time solution')
