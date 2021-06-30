f875765c9f3263d464d2b9421c9c54fb7de6971c
Hendrik Kleikamp <h_klei18@uni-muenster.de>
2021-06-30 12:40:32 +0200

    added Engquist-Osher flux

--- examples/linear_transport_1d.py
+++ examples/linear_transport_1d.py
@@ -2,30 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from tent_pitching import perform_tent_pitching
-from tent_pitching.grids import Vertex, Element, Grid
+from tent_pitching.grids import create_uniform_grid
 from tent_pitching.utils.visualization import (plot_space_time_grid, plot_space_function,
                                                plot_space_time_function)
 from tent_pitching.operators import GridOperator
 from tent_pitching.functions import DGFunction
-from tent_pitching.discretizations import DiscontinuousGalerkin, LaxFriedrichsFlux
+from tent_pitching.discretizations import DiscontinuousGalerkin, EngquistOsherFlux
 
 
-vertex0 = Vertex(0., label="Vertex 0")
-vertex1 = Vertex(0.25, label="Vertex 1")
-vertex2 = Vertex(0.75, label="Vertex 2")
-vertex3 = Vertex(1., label="Vertex 3")
-element0 = Element(vertex0, vertex1, label="Element 0")
-element1 = Element(vertex1, vertex2, label="Element 1")
-element2 = Element(vertex2, vertex3, label="Element 2")
-elements = [element0, element1, element2, ]
-grid = Grid(elements)
+grid = create_uniform_grid(1.)
 T_MAX = 1.
 MU = 1.
-EPS = 2e-0
 
 
 def characteristic_speed(x):
-    return MU + EPS
+    return MU
 
 
 space_time_grid = perform_tent_pitching(grid, T_MAX, characteristic_speed, n_max=1000)
@@ -44,12 +35,22 @@ def linear_transport_flux_derivative(u):
     return MU
 
 
+def linear_transport_positive_flux(u):
+    return max(u, 0.)
+
+
+def linear_transport_negative_flux(u):
+    return min(u, 0.)
+
+
 def inverse_transformation(u, phi_2, phi_2_dt, phi_2_dx):
     return u / (1. - phi_2_dx * MU)
 
 
 lambda_ = LOCAL_TIME_GRID_SIZE / LOCAL_SPACE_GRID_SIZE
-numerical_flux = LaxFriedrichsFlux(linear_transport_flux, linear_transport_flux_derivative, lambda_)
+numerical_flux = EngquistOsherFlux(linear_transport_flux,
+                                   flux_positive=linear_transport_positive_flux,
+                                   flux_negative=linear_transport_negative_flux)
 
 discretization = DiscontinuousGalerkin(numerical_flux, inverse_transformation,
                                        LOCAL_SPACE_GRID_SIZE)
@@ -57,6 +58,7 @@ discretization = DiscontinuousGalerkin(numerical_flux, inverse_transformation,
 
 def u_0_function(x, jumps=True):
     if jumps:
+        return 1. * (x <= 0.25)
         return 1. * (x <= 0.2) + 0.5 * (0.2 < x <= 0.5)
     return 0.5 * (1.0 + np.cos(2.0 * np.pi * x)) * (0.0 <= x <= 0.5) + 0. * (x > 0.5)
 
--- tent_pitching/discretizations/numerical_fluxes.py
+++ tent_pitching/discretizations/numerical_fluxes.py
@@ -1,7 +1,10 @@
 class NumericalFlux:
-    def __init__(self, flux, flux_derivative=None, lambda_=None):
+    def __init__(self, flux, flux_derivative=None, flux_positive=None, flux_negative=None,
+                 lambda_=None):
         self.flux = flux
         self.flux_derivative = flux_derivative
+        self.flux_positive = flux_positive
+        self.flux_negative = flux_negative
         self.lambda_ = lambda_
 
 
@@ -14,3 +17,9 @@ class LaxFriedrichsFlux(NumericalFlux):
 class BackwardDifference(NumericalFlux):
     def __call__(self, u_1, u_2):
         return self.flux(u_1)
+
+
+class EngquistOsherFlux(NumericalFlux):
+    def __call__(self, u_1, u_2):
+        assert self.flux_positive is not None and self.flux_negative is not None
+        return self.flux_positive(u_1) + self.flux_negative(u_2)
