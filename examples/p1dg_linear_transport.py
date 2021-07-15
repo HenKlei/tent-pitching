fixed error computation and adjusted Burgers equation example

examples/p1dg_linear_transport.py
@@ -4,10 +4,11 @@ import numpy as np
 from typer import Option, run
 
 from tent_pitching import perform_tent_pitching
-from tent_pitching.functions import SpaceTimeFunction
 from tent_pitching.grids import create_uniform_grid
 from tent_pitching.operators import GridOperator
-from tent_pitching.utils.visualization import plot_space_time_function, write_space_time_grid
+from tent_pitching.utils.error_computations import compute_error
+from tent_pitching.utils.visualization import (plot_space_time_function, write_space_time_grid,
+                                               plot_space_time_function_difference)
 
 
 T_MAX = 1.
@@ -26,11 +27,6 @@ def main(mu: float = Option(1., help='Parameter mu that determines the velocity'
     def characteristic_speed(x):
         return mu + EPS
 
-    space_time_grid = perform_tent_pitching(grid, T_MAX, characteristic_speed, n_max=None)
-
-    write_space_time_grid(space_time_grid, FILEPATH_RESULTS +
-                          f'grid_linear_transport_mu_{str(mu).replace(".", "_")}_n_{n}')
-
     def linear_transport_flux(u):
         return np.array([mu * u, ])
 
@@ -45,6 +41,11 @@ def main(mu: float = Option(1., help='Parameter mu that determines the velocity'
     def inflow_boundary_values(x):
         return 1.
 
+    space_time_grid = perform_tent_pitching(grid, T_MAX, characteristic_speed, n_max=None)
+
+    write_space_time_grid(space_time_grid, FILEPATH_RESULTS +
+                          f'grid_linear_transport_mu_{str(mu).replace(".", "_")}_n_{n}')
+
     grid_operator = GridOperator(space_time_grid, linear_transport_flux,
                                  linear_transport_flux_derivative,
                                  u_0_function, inflow_boundary_values)
@@ -57,15 +58,15 @@ def main(mu: float = Option(1., help='Parameter mu that determines the velocity'
     def exact_solution(x):
         return u_0_function(x[0] - mu * x[1])
 
-    exact_solution_space_time_function = SpaceTimeFunction(space_time_grid)
-    exact_solution_space_time_function.interpolate(exact_solution)
-    plot_error = plot_space_time_function(u - exact_solution_space_time_function)
+    plot_solution = plot_space_time_function(exact_solution)
+    plot_solution.savefig(FILEPATH_RESULTS
+                          + f'exact_solution_mu_{str(mu).replace(".", "_")}_n_{n}_global.pdf')
+
+    plot_error = plot_space_time_function_difference(u, exact_solution)
     plot_error.savefig(FILEPATH_RESULTS +
                        f'u_mu_{str(mu).replace(".", "_")}_n_{n}_global_error.pdf')
 
-    absolute_error = (u - exact_solution_space_time_function).two_norm()
-    norm_exact_solution = exact_solution_space_time_function.two_norm()
-    relative_error = absolute_error / norm_exact_solution
+    relative_error, absolute_error = compute_error(u, exact_solution)
 
     with open(FILEPATH_RESULTS + 'errors_linear_transport_st_dg_p1.txt', 'a') as file_obj:
         file_obj.write(f"{mu}\t{n}\t{len(space_time_grid.tents)}\t"

tent_pitching/operators/grid_operator.py
@@ -38,7 +38,7 @@ class GridOperator:
         return function
 
     def solve_local_problem(self, tent, solution_on_inflow_tents,
-                            tol=1e-6, max_iter=None):
+                            tol=1e-6, max_iter=1000):
         assert tent in self.space_time_grid.tents
         # local_initial_value has to be list of LocalSpaceFunctions
 
@@ -118,6 +118,7 @@ class GridOperator:
 
         iteration = 0
         while np.linalg.norm(res) > tol and (max_iter is None or iteration < max_iter):
+            logger.info(f"Iteration {iteration}, residual norm {np.linalg.norm(res):.3e} ...")
             mat = self.compute_matrix(tent, local_solution)
             update = np.linalg.solve(mat, -res)
             local_solution = local_solution + update

tent_pitching/utils/visualization.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
 
 from tent_pitching.grids import SpaceTimeGrid
-from tent_pitching.functions.global_functions import SpaceTimeFunction
 
 
 def plot_space_time_grid(space_time_grid, title=''):
@@ -59,7 +58,6 @@ def plot_space_function(u, title='', n=100):
 
 def plot_space_time_function(u, title='', interval=1, three_d=False, space_time_grid=None,
                              nx=50, ny=50):
-    assert isinstance(u, SpaceTimeFunction)
     assert space_time_grid is None or isinstance(space_time_grid, SpaceTimeGrid)
 
     fig = plt.figure()
@@ -74,6 +72,64 @@ def plot_space_time_function(u, title='', interval=1, three_d=False, space_time_
     uv = np.zeros(xv.flatten().shape)
     for i, (x, y) in enumerate(zip(xv.flatten(), yv.flatten())):
         uv[i] = u(np.array([x, y]))
+
+    min_val = np.min(uv)
+    max_val = np.max(uv)
+
+    if three_d:
+        scatter = axes.scatter(xv, yv, uv, c=uv, vmin=min_val, vmax=max_val)
+    else:
+        scatter = axes.scatter(xv, yv, c=uv, vmin=min_val, vmax=max_val)
+
+    fig.colorbar(scatter)
+
+    if space_time_grid and three_d:
+        import mpl_toolkits.mplot3d.art3d as art3d
+        for tent in space_time_grid.tents:
+            for vertex1 in tent.space_time_vertices:
+                for vertex2 in tent.space_time_vertices:
+                    if (vertex1 != vertex2 and
+                            space_time_grid.check_space_time_vertices_adjacent(vertex1, vertex2)):
+                        if vertex1.space_vertex == vertex2.space_vertex:
+                            line = art3d.Line3D([vertex1.coordinates[0], vertex2.coordinates[0]],
+                                                [vertex1.coordinates[1], vertex2.coordinates[1]],
+                                                [0., 0.],
+                                                linewidth=0.5, linestyle='--', color='green')
+                        else:
+                            line = art3d.Line3D([vertex1.coordinates[0], vertex2.coordinates[0]],
+                                                [vertex1.coordinates[1], vertex2.coordinates[1]],
+                                                [0., 0.],
+                                                linewidth=1, linestyle='-', color='blue')
+                        axes.add_line(line)
+
+        for vertex in space_time_grid.space_time_vertices:
+            axes.plot(*vertex.coordinates, marker='o', color='lightblue')
+
+    axes.set_xlabel('x')
+    axes.set_ylabel('t')
+    if not three_d:
+        axes.set_aspect('equal')
+    axes.set_title(title)
+
+    return fig
+
+
+def plot_space_time_function_difference(u, v, title='', interval=1, three_d=False,
+                                        space_time_grid=None, nx=50, ny=50):
+    assert space_time_grid is None or isinstance(space_time_grid, SpaceTimeGrid)
+
+    fig = plt.figure()
+    if three_d:
+        axes = fig.add_subplot(1, 1, 1, projection='3d')
+    else:
+        axes = fig.add_subplot(1, 1, 1)
+
+    xs = np.linspace(0., 1., nx)
+    ys = np.linspace(0., 1., ny)
+    xv, yv = np.meshgrid(xs, ys)
+    uv = np.zeros(xv.flatten().shape)
+    for i, (x, y) in enumerate(zip(xv.flatten(), yv.flatten())):
+        uv[i] = u(np.array([x, y])) - v(np.array([x, y]))
 
     min_val = np.min(uv)
     max_val = np.max(uv)